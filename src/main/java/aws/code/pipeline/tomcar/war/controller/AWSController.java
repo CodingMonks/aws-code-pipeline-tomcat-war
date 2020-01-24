73b4318d49c7d5fee65e9ce5645e7066c5f0a3c4
CodingMonks <tech4saurabh@gmail.com>
2020-01-24 00:54:29 -0500

    spacing in yml corrected

--- src/main/java/aws/code/pipeline/tomcar/war/controller/AWSController.java
+++ src/main/java/aws/code/pipeline/tomcar/war/controller/AWSController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
-@RequestMapping("/build-status")
+@RequestMapping("/status")
 @RestController
 public class AWSController {
 
